ba419b8e590554309e34cf7d1b68023b3a35bae8
Wander Torres <wander_torres@live.com>
2022-03-23 00:45:00 -0300

    implements contact page

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { Button } from "../../components/Button";
 
 export const Contact = () => {
     return(
-        <section>
-            <h1>Contato</h1>
-            <form>
-                <input type="text" />
-                <input type="text" />
-                <textarea name="" id=""></textarea>
+        <section className="contact-container">
+            <header className="contact-header">
+                <h1 className="header-title">Contato</h1>
+                <div className="line-container">
+                    <div className="line-base"></div>
+                    <div className="line-effect"></div>
+                </div>
+            </header>
+
+            <form className="contact-form">
+                <div className="form-input_area">
+                    <input type="text" />
+                    <input type="text" />
+                </div>
+                <textarea className="form-text_area" name="" id=""></textarea>
+                <Button title="Salvar" link="/" type="--form" />
             </form>
         </section>
     );
