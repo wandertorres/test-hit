d9e3d0ef63de9cbe1652b1d7b53ab588db7f366c
Wander Torres <wander_torres@live.com>
2022-03-22 22:22:36 -0300

    implements header section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,9 +2,16 @@ import React from "react";
 
 export const Home = () => {
     return(
-        <section className="section-container">
-            <h1>Introdução</h1>
-            <p>Texto</p>
+        <section className="home-container">
+            <header className="home-header">
+                <h1 className="header-title">Introdução</h1>
+                <div className="line-container">
+                    <div className="line-base"></div>
+                    <div className="line-effect"></div>
+                </div>
+            </header>
+
+            <p className="header-text">Texto</p>
         </section>
     );
 }
